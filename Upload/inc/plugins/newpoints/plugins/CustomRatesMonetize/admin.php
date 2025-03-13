702890ece57d77e1956d31fd48487afec079856f
Sama34 <Sama34@users.noreply.github.com>
2025-03-12 18:22:38 -0700

    META 3.1.3

--- Upload/inc/plugins/newpoints/plugins/CustomRatesMonetize/admin.php
+++ Upload/inc/plugins/newpoints/plugins/CustomRatesMonetize/admin.php
@@ -118,8 +118,8 @@ function plugin_information(): array
         'website' => 'https://ougc.network',
         'author' => 'Omar G.',
         'authorsite' => 'https://ougc.network',
-        'version' => '3.1.2',
-        'versioncode' => 3102,
+        'version' => '3.1.3',
+        'versioncode' => 3103,
         'compatibility' => '31*',
         'codename' => 'newpoints_custom_rates_monetiz'
     ];
